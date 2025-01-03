5a93b5e3377c9adcd6e8fd28201a40a2d0c1e12a
Lê Chí Anh <lechianh04@gmail.com>
2025-01-03 13:54:16 +0700

    First commit

---
+++ test.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+import time
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Đường dẫn tới EdgeDriver
+service = Service("E:/NLP project/edgedriver_win64/msedgedriver.exe")
+driver = webdriver.Edge(service=service)
+
+# URL của fanpage
+fanpage_url = "https://www.facebook.com/thongtinchinhphu"
+driver.get(fanpage_url)
+
+
+
+# Chờ trang tải ban đầu
+time.sleep(500)
+
+/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[8]/div/div/div/div/div/div/div/div/div/div/div/div[13]/div/div/div[3]/div[1]/div/div/div/div/span/div[3]/div/div
+
+
+x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f
+
+//*[@id=":rc2:"]/div/div/span/div[3]/div/div
